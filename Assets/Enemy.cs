49c37ab5097fa1ad688a7b8e1fe1f68c4f7088d6
ryotakonishi <ryota.konishi@icloud.com>
2025-07-07 19:07:44 +0900

    [add]変数追加

--- Assets/Enemy.cs
+++ Assets/Enemy.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     [NonSerialized] public int EnemyHP;
+
+    [SerializeField] int EnemySpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
