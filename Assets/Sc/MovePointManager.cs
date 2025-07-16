e5a6b1f5c978085ce3c07f0aa2e25b8f9dc154d5
ryotakonishi <ryota.konishi@icloud.com>
2025-07-17 03:58:46 +0900

    [add]変数名変更

--- Assets/Sc/MovePointManager.cs
+++ Assets/Sc/MovePointManager.cs
@@ -1,23 +1,21 @@
-using System;
 using UnityEditor;
 using UnityEngine;
-using static UnityEngine.GraphicsBuffer;
 
 #if UNITY_EDITOR
 [CustomEditor(typeof(MovePoint))]
 public class MovePointManager : Editor
 {
-    MovePoint movepoint => target as MovePoint;
+    MovePoint _movepoint => target as MovePoint;
 
     private void OnSceneGUI()
     {
         Handles.color = Color.yellow;
 
-        for (int i = 0; i < movepoint.points.Length; i++)
+        for (int i = 0; i < _movepoint.points.Length; i++)
         {
             EditorGUI.BeginChangeCheck();//EndCheckChangeとの間で変更が行われてるかチェック
-            Vector3 currentpoint = movepoint.points[i];//位置取得
-            Vector3 newpoint = Handles.FreeMoveHandle
+            Vector3 currentpoint = _movepoint.points[i];//位置取得
+            Vector3 newpoint = Handles.FreeMoveHandle//SceneでこのGUIを動かすために使われてるメソッド
                 (currentpoint, 0.7f, new Vector3(0.3f, 0.3f, 0.3f), Handles.SphereHandleCap);
 
             GUIStyle textStyle = new();
@@ -25,14 +23,14 @@ public class MovePointManager : Editor
             textStyle.normal.textColor = Color.white;
             Vector3 textpos = Vector3.down * 0.35f + Vector3.right * 0.35f;
 
-            Handles.Label(movepoint.points[i] + textpos, $"{i + 1}", textStyle);
+            Handles.Label(_movepoint.points[i] + textpos, $"{i + 1}", textStyle);
 
-            EditorGUI.EndChangeCheck();
+            EditorGUI.EndChangeCheck();//BeginChangeCheckとの間で変更が行われてたらtrue
 
             if (EditorGUI.EndChangeCheck())
             {
                 Undo.RecordObject(target, "Move");
-                movepoint.points[i] = newpoint;
+                _movepoint.points[i] = newpoint;
             }
         }
     }
