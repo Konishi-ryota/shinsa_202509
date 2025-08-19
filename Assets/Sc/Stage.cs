1681191f77e0f1ee420da397547f26512ed28d93
ryotakonishi <ryota.konishi@icloud.com>
2025-08-19 14:50:52 +0900

    [add]謎の空白が追加されてた

--- Assets/Sc/Stage.cs
+++ Assets/Sc/Stage.cs
@@ -5,12 +5,13 @@ public class Stage : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
+    
